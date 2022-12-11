02c869c4c954df79eb2dfe433d1ef222b3131bc4
Erdinch <erdinchorhan02@gmail.com>
2022-12-11 20:00:10 +0200

    passport

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const session = require('express-session');
 const passport = require('passport');
 const localStrategy = require('passport-local').Strategy;
 const mongoose = require('mongoose');
-require('dotenv').config();
+// routes
+const signUpRouter = require('./routes/signUp');
+const logInRouter = require('./routes/logIn');
+// models
+const User = require('./models/user');
 
 const mongoDb = process.env.MONGODB_URL;
 mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
@@ -23,14 +28,41 @@ app.use(
     saveUninitialized: true,
   })
 );
+
+passport.use(
+  new localStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username' });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: 'Incorrect password' });
+      }
+      return done(null, user);
+    });
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
+
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-const signUpRouter = require('./routes/signUp');
-
 app.get('/', (req, res) => res.render('index'));
 app.use('/sign-up', signUpRouter);
+app.use('/log-in', logInRouter);
 
 const PORT = process.env.PORT || 5000;
 
