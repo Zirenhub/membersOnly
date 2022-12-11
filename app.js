4d7caa10e047ad914fd0e8284c76e45045d9b69a
Erdinch <erdinchorhan02@gmail.com>
2022-12-11 17:20:18 +0200

    port, routes

--- app.js
+++ app.js
@@ -8,5 +8,30 @@ require('dotenv').config();
 
 const mongoDb = process.env.MONGODB_URL;
 mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
-const db = mongoose.connection();
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongo connection error'));
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+const signUpRouter = require('./routes/signUp');
+
+app.get('/', (req, res) => res.render('index'));
+app.use('/sign-up', signUpRouter);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
